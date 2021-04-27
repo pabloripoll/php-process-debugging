b9e3112424ff43e191f7fa596863ee036890ee9e
Pablo Ripoll <pabloripoll.it@gmail.com>
2021-04-27 14:34:13 +0200

    Create debugger.php

---
+++ debugger.php
@@ -0,0 +1,14 @@
+```php
+<?php
+
+/*
+  ** DEBUGGER
+  * $debugContent must be array or object
+*/
+$debugContent = print_r([$variable], true);
+$debugFile = '/var/www/html/{project-direcotry}/debug.log';
+file_put_contents($debugFile, "\n[".date("Y.m.d H:i:s")."]↴\n".$debugContent, FILE_APPEND | LOCK_EX");
+/* */
+
+?>
+```
